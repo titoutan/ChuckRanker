1b3961937347b80f51ad867af0519c783b079bca
Titouan <titouan.brando@etu.iut-tlse3.fr>
2024-02-12 09:17:19 +0100

    Feat: style & animations

--- src/components/JeuChoix.vue
+++ src/components/JeuChoix.vue
@@ -1,8 +1,8 @@
 <template>
   <h3>Choissez la phrase que vous préferrez</h3>
   <div id="phrases">
-    <p class="phrase" ref="phrase1" @click="choisir(1)" :class="{choisi: choix===1, paschoisi: choix===2, jeu: choix===0}">{{ phrase1.phrase }} <span v-if="choix !== 0">({{ phrase1.vote }})</span></p>
-    <p class="phrase" ref="phrase2" @click="choisir(2)" :class="{choisi: choix===2, paschoisi: choix===1, jeu: choix===0}">{{ phrase2.phrase }} <span v-if="choix !== 0">({{ phrase2.vote }})</span></p>
+    <p class="phrase" ref="phrase1" @click="choisir(1)" :class="{choisi: choix===1, paschoisi: choix===2, jeu: choix===0, degage: degager1, inserer:inserer1}">{{ phrase1.phrase }} <span v-if="choix !== 0">({{ phrase1.vote==null? 0 : phrase1.vote }})</span></p>
+    <p class="phrase" ref="phrase2" @click="choisir(2)" :class="{choisi: choix===2, paschoisi: choix===1, jeu: choix===0, degage: degager2, inserer:inserer2}">{{ phrase2.phrase }} <span v-if="choix !== 0">({{ phrase2.vote==null? 0 : phrase2.vote }})</span></p>
   </div>
   <p v-if="choix !== 0"><span class="highlight">{{ stats }}%</span> des gens sont de votre avis !</p>
   <button @click="continuer()" v-if="choix !== 0">Continuer</button>
@@ -17,6 +17,10 @@ export default {
       phrases: null,
       phrase1: "",
       phrase2: "",
+      degager1: true,
+      degager2: true,
+      inserer1: true,
+      inserer2: true,
       choix: 0
     }
   },
@@ -45,11 +49,16 @@ export default {
       do {
         this.phrase1 = this.phrases.data[Math.floor(Math.random() * this.phrases.data.length)];
       } while(this.phrase1 === this.phrase2)
+      this.degager1 = false
+      this.inserer1 = true
+
     },
     genererPhrase2() {
       do {
         this.phrase2 = this.phrases.data[Math.floor(Math.random() * this.phrases.data.length)];
       } while(this.phrase1 === this.phrase2)
+      this.degager2 = false
+      this.inserer2 = true
     },
     async voter(phrase) {
       phrase.vote = parseInt(phrase.vote==null? 0 : phrase.vote)+1;
@@ -75,12 +84,20 @@ export default {
       }
     },
     continuer() {
+      document.getElementsByClassName("paschoisi")[0].classList.add("degage")
+      console.log(document.getElementsByClassName("paschoisi")[0])
       switch (this.choix) {
         case 1:
-          this.genererPhrase2()
+          setTimeout(this.genererPhrase2,
+          500);
+          this.inserer2 = false;
+          this.degager2 = true;
           break;
         case 2:
-          this.genererPhrase1()
+          setTimeout(this.genererPhrase1,
+          500);
+          this.inserer1 = false;
+          this.degager1 = true;
           break;
       }
       this.choix = 0;
@@ -104,9 +121,13 @@ h3 {
   padding: 30px;
   font-size: 1.8em;
   transition: all 0.3s;
+  cursor: pointer;
+  transition-duration: 300ms;
+  transition-property: transform;
 }
 .jeu:hover {
   background-color: #42b983;
+  transform: translateY(-5px);
 }
 .paschoisi {
   border: 2px solid #498067;
@@ -115,6 +136,10 @@ h3 {
   background-color: #42b983;
   border: 2px solid #42b983;
 }
+.highlight {
+  color: #42b983;
+  font-weight: 700;
+}
 button {
   font-size: 1.9em;
   padding: 50px;
@@ -122,5 +147,12 @@ button {
   border:none;
   border-radius: 70px;
   box-shadow: 4px 5px 5px black;
+  cursor: pointer;
 }
-</style>
+.degage {
+  transform: translateX(-150%);
+}
+.inserer {
+  transform: translateX(150%), translateX(-150%);
+}
+</style>  
